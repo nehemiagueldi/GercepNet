93a18d90159c4b8aec2f54cae35929e07e0624fd
Joannekess <joannekessie@gmail.com>
2021-11-30 01:51:02 +0700

    welcome 1/2

--- gercepnet/resources/views/home.blade.php
+++ gercepnet/resources/views/home.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=This is, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/template/main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+    <title>Welcome Page</title>
 </head>
 <body>
-    This is home Page
+    <div class="container">
+        <h5 class="louis text-center mt-5">Welcome to our website</h1>
+    </div>
 </body>
 </html>
