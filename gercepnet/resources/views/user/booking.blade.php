9766c70bcfeeec2b005704e2418427b282a17b41
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-12-02 00:47:42 +0700

    Update booking.blade.php

--- gercepnet/resources/views/user/booking.blade.php
+++ gercepnet/resources/views/user/booking.blade.php
@@ -7,4 +7,19 @@
 
 @section('container')
     <h1>{{ $title }}</h1>
+
+    <form action="/login" method="POST">
+        <div class="form-floating">
+          <select type="room" name="email" class="" id="email" placeholder="name@example.com" autofocus value="">
+          <label for="email">Email address</label>
+              <div class="invalid-feedback">
+              </div>
+        </div>
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="" placeholder="">
+          <label for="password">Password</label> 
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
+      </form>
+
 @endsection
