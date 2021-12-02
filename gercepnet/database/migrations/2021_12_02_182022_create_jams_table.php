5cd8288fa4bab094b9251a76dc41e62d4d8154a8
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-12-03 01:24:54 +0700

    Tambahan

---
+++ gercepnet/database/migrations/2021_12_02_182022_create_jams_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jams', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jams');
+    }
+}
