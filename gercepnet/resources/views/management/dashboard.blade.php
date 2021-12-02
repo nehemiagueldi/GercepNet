8ca8c7949ac74e4d8d2b109f0acebe033b490e8d
Ploeroe <raphael.hakim@student.umn.ac.id>
2021-12-03 01:18:38 +0700

    Update

--- gercepnet/resources/views/management/dashboard.blade.php
+++ gercepnet/resources/views/management/dashboard.blade.php
@@ -7,7 +7,7 @@
 @include('navbar.managenavbar')
 
 @section('container')
-    <h3 class="minititle louis pt-5">Welcome to our website</h2>
+    <h3 class="minititle louis pt-5">Welcome back</h2>
     <h1 class="title">MANAGEMENT</h1>
     <h3 class="minititle louis pt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer \
